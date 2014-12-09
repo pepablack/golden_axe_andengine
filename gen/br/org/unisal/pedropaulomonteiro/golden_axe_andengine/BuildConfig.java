5f3c3e30b750d0c11c3b9d1bff9625f6d116d19e
Pedro Paulo Monteiro <pepablack@gmail.com>
2014-12-09 19:59:36 -0200

    First Commit
    First Commit

---
+++ gen/br/org/unisal/pedropaulomonteiro/golden_axe_andengine/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.org.unisal.pedropaulomonteiro.golden_axe_andengine;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
